a8d70198e68ee734bfc00134b676df0d90018e45
Aishwarya Shrikant Mundada <amundada@mtu.edu>
2020-03-31 19:03:45 -0400

    matching2D edited for Project

--- src/matching2D.hpp
+++ src/matching2D.hpp
@@ -19,7 +19,6 @@
 
 #include "dataStructures.h"
 
-
 /*void detKeypointsHarris(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool bVis=false);
 void detKeypointsShiTomasi(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool bVis=false);
 void detKeypointsModern(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, std::string detectorType, bool bVis=false);
